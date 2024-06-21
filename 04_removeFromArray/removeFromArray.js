6654345c2b38ca8369afb908d170d6c54a216ee4
Pentaganos <3890060+Pentaganos@users.noreply.github.com>
2024-06-21 21:16:04 +0800

    refactor: Modify removeFromArray function to accept multiple arguments for removal

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, args) {
+    let argsArray = Array.from(arguments).slice(1);
+    return array.filter(item => !argsArray.includes(item));
 };
 
 // Do not edit below this line
