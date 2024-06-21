4efdd4f714a9038d11a3715768c99a11dba6ff7d
Pentaganos <3890060+Pentaganos@users.noreply.github.com>
2024-06-21 21:18:11 +0800

    feat: Improve sumAll function to handle negative numbers and non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
 const sumAll = function() {
-
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] !== 'number' || arguments[i] < 0) {
+        return 'ERROR';
+        }
+    }
+    if (arguments[0] < arguments[1]) {
+        for (let i = arguments[0]; i <= arguments[1]; i++) {
+        sum += i;
+        }
+    } else {
+        for (let i = arguments[1]; i <= arguments[0]; i++) {
+        sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
