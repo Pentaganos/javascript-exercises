125c310292ca335fda4d7c33410c339598b31272
Pentaganos <3890060+Pentaganos@users.noreply.github.com>
2024-06-21 21:33:47 +0800

    feat: Improve leapYears and tempConversion functions
    Refactor the leapYears function to accept a year parameter and return a boolean indicating whether it is a leap year. Also, modify the tempConversion functions to accept a temperature parameter and convert it to Celsius or Fahrenheit respectively. These changes improve the functionality and flexibility of the code.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  //computes the sum of an array of numbers
+  // if array is empty, return 0
+  if (array.length === 0) {
+    return 0;
+  }
+  // if array has one number, return that number
+  if (array.length === 1) {
+    return array[0];
+  }
+  // if array has more than one number, use reduce to add them
+  return array.reduce((acc, num) => acc + num);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  //multiply an array of numbers
+  // if array is empty, return 0
+  if (array.length === 0) {
+    return 0;
+  }
+  // if array has one number, return that number
+  if (array.length === 1) {
+    return array[0];
+  }
+  // if array has more than one number, use reduce to multiply them
+  return array.reduce((acc, num) => acc * num);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	// function for calculating factorial of number
+  // if number is 0, return 1
+  if (num1 === 0) {
+    return 1;
+  }
+  // if number is 1, return 1
+  if (num1 === 1) {
+    return 1;
+  }
+  // if number is greater than 1, use recursion to calculate factorial
+  return num1 * factorial(num1 - 1);
 };
 
 // Do not edit below this line
