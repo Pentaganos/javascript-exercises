0819cd7f531c058a4297399cfc664c10dea95725
Pentaganos <3890060+Pentaganos@users.noreply.github.com>
2024-06-24 18:41:13 +0800

    Learning Learning JavaScript

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    if (number == 0) return 0;
+    if (number === 1) return 1;
+    let a = 0;
+    let b = 1;
+    let c = 1;
+    for (let i = 2; i <= number; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(oldest) {
+    return oldest.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+    });  
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
